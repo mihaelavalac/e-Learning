77004daa372095b79a9c77b49357820239a7142c
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-16 11:41:09 -0500

    Added some style to the home page.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,6 @@ router.get('/', (req, res) => {
     attributes: ["id", "title", "description"],
   }).then(dbCourseData => {
     // pass a single post object into the homepage template
-    console.log(dbCourseData[0]);
     const courses = dbCourseData.map(course => course.get({ plain: true }));
 
     res.render('homepage', { courses });
