d6e251fb908bd9c6f6d480bfeef606f7c6b28b47
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-16 13:40:16 -0500

    Created the  login/sigup forms and the session.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,4 +17,20 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render('login');
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
 module.exports = router;
